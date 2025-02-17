f82a0f512375a6186b02a46cbd99b93370eebce6
Chiarinze <arinzec2000@gmail.com>
2025-02-17 09:11:06 +0100

    completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,17 +4,17 @@ export default function Footer() {
   const contactIcons = [
     {
       name: "Facebook",
-      icon: <Facebook size={32} />,
+      icon: <Facebook size={22} />,
       link: "https://web.facebook.com/BcsNig/?_rdc=1&_rdr#",
     },
     {
       name: "Instagram",
-      icon: <Instagram size={32} />,
+      icon: <Instagram size={22} />,
       link: "https://www.instagram.com/the_benin_chorale_society/?igsh=MXVybmpseXkxOGQwZg%3D%3D",
     },
     {
       name: "Youtube",
-      icon: <Youtube size={32} />,
+      icon: <Youtube size={22} />,
       link: "https://www.youtube.com/@beninchoraleandphilharmonic?app=desktop&si=7qcI3YtsV7aVOYkL",
     },
   ];
@@ -24,18 +24,18 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center gap-8">
           <div>
-            <h3 className="text-xl font-bold mb-4">BCS</h3>
-            <p className="text-gray-400">
+            <h3 className="text-lg font-bold mb-4">BCS</h3>
+            <p className="text-gray-400 text-sm">
               Inspiring musical excellence since 2012
             </p>
           </div>
           <div>
-            <h3 className="text-xl font-bold mb-4">Contact</h3>
-            <p className="text-gray-400">Benin City, Edo State</p>
-            <p className="text-gray-400">Nigeria</p>
+            <h3 className="text-lg font-bold mb-4">Contact</h3>
+            <p className="text-gray-400 text-sm">Benin City, Edo State</p>
+            <p className="text-gray-400 text-sm">Nigeria</p>
           </div>
           <div>
-            <h3 className="text-xl font-bold mb-4">Follow Us</h3>
+            <h3 className="text-lg font-bold mb-4">Follow Us</h3>
             
             <div className="flex space-x-4">
             {contactIcons.map((contact, index) => (
@@ -44,7 +44,7 @@ export default function Footer() {
                 href={contact.link}
                 target={contact.name !== "Phone" ? "_blank" : "_self"}
                 rel="noopener noreferrer"
-                className="w-6 h-6 text-gray-400"
+                className="w-2 h-2 text-gray-400"
               >
                 {contact.icon}
               </a>
@@ -52,7 +52,7 @@ export default function Footer() {
           </div>
           </div>
         </div>
-        <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
+        <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400 text-sm">
           <p>
             &copy; {new Date().getFullYear()} Benin Chorale & Philharmonic
             Nigeria. All rights reserved.
