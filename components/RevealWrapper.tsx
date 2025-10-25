d56c6ee30597ab750f2924ff265248d60a6e1d2a
Chiarinze <arinzec2000@gmail.com>
2025-10-25 23:32:36 +0100

    Nothing

--- components/RevealWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { ReactNode } from "react";
-import useReveal from "@/hooks/useReveal";
-
-export function RevealWrapper({ children }: { children: ReactNode }) {
-  useReveal();
-  return <>{children}</>;
-}
--- global.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@supabase/supabase-js';
-
-declare module "pdfkit/js/pdfkit.standalone.js" {
-  import PDFDocument from "pdfkit";
-  export default PDFDocument;
-}
